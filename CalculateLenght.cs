60a692cc38a5f965ccd4e67905ea8af9587c6478
snehal979 <113917713+snehal979@users.noreply.github.com>
2022-11-25 14:16:17 +0530

    Add files via upload

---
+++ CalculateLenght.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionPart
+{
+    public class CalculateLenght
+    {
+        public static double LenghtOfLine(int x1, int y1, int x2,int y2)
+        {
+            double result = (x2- x1)^2 + (y2-y1)^2;
+            double c = Math.Sqrt(result);
+            return c;
+        } 
+       
+    }
+}
